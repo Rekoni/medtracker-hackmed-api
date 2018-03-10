acfb929ab686d5853f3b2d3699970dab0ac57105
raresmei <mcrares@yahoo.com>
2018-03-10 21:29:09 +0000

    Trimmed

--- api/routes/DatabaseRoutes.js
+++ api/routes/DatabaseRoutes.js
@@ -1,40 +1,8 @@
 require('dotenv/config');
 const bodyParser = require('body-parser');
-
 const resHelpers = require('../helpers/ResponseHelpers');
 const dbController = require('../controllers/DatabaseController');
 
 module.exports = (app) => {
     app.use(bodyParser.urlencoded({ extended: false }));
-
-    app.post('/timetable/offset', dbController.offsetTimetable);
-
-
-    try {
-        const adminController = require('../controllers/AdminController');
-
-        app.post('/exercise', (req, res, next) => {
-            if (!adminController) {
-                return res.status(403).send(
-                    resHelpers.error("This instance of the API does not" +
-                        "have permission to use this method!")
-                );
-            }
-            adminController.connectToAdminUser();
-            next();
-        }, adminController.createExercise);
-    
-        app.post('/timetable', (req, res, next) => {
-            if (!adminController) {
-                return res.status(403).send(
-                    resHelpers.error("This instance of the API does not" +
-                        "have permission to use this method!")
-                );
-            }
-            adminController.connectToAdminUser();
-            next();
-        }, adminController.createEvent);
-    } catch (err) {
-        console.log("No admin controller!");
-    }
 };
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-
+const dbRoutes = require('./api/routes/DatabaseRoutes.js');
+const userRoutes = require('./api/routes/UserRoutes.js');
 const port = process.env.PORT || 5000;
 const app = express();
 
