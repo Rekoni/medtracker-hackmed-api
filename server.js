edf51f73139265533b0fcf2a2adad7e90b742c38
raresmei <mcrares@yahoo.com>
2018-03-11 03:02:41 +0000

    Send SMS

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dbRoutes = require('./api/routes/DatabaseRoutes.js');
 const userRoutes = require('./api/routes/UserRoutes.js');
 const port = process.env.PORT || 5000;
 const app = express();
-
+const smsHelpers = require('./api/helpers/SmsHelpers.js');
 mongoose.connect("mongodb://" + process.env.DB_USER + ":" +
         process.env.DB_PASSWORD + "@" + process.env.DB_NAME);
 app.use(morgan('dev'));
@@ -25,3 +25,4 @@ userRoutes(app);
 app.listen(port);
 
 console.log('Medtracker started on port: ' + port);
+smsHelpers.sendMessage('447468898220', "Nexmo works yay");
